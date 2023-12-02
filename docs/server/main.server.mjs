f35b624f593fa7722772ddaabbbdf0858e039636
Hildemir <hildemir.regis@hotmail.com>
2023-12-02 18:55:02 -0300

    Fixed responsiveness bugs

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-EEBCPEML.mjs";import"./chunk-TBKX7ISY.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-KZP4FXIR.mjs";import"./chunk-TBKX7ISY.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
